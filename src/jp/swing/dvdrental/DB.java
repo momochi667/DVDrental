8dce6ffc6316b6023c6acf640e1f8f6e93558563
mid <mid@KCSK3232.k302.kcsk.ac.jp>
2025-07-14 09:32:57 +0900

    更新

---
+++ src/jp/swing/dvdrental/DB.java
@@ -0,0 +1,8 @@
+package jp.swing.dvdrental;
+
+public class DB {
+	static final String URL = "jdbc:mysql://localhost/rentaldb";
+	static final String USER = "root";
+	static final String PASS = "";
+	//DBアクセス＆ユーザー名とパスワード
+}
