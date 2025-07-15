4c8a32e4fbd8ddafd1a361cafde3069fc7603063
mid <mid@KCSK3232.k302.kcsk.ac.jp>
2025-07-15 11:24:35 +0900

    update

--- src/jp/swing/dvdrental/TopPanel.java
+++ src/jp/swing/dvdrental/TopPanel.java
@@ -10,11 +10,11 @@ public class TopPanel extends JPanel {
 	       setLayout(new GridLayout(6, 1, 10, 10));
 	       
 	       //会員パネルに移動　インスタンス名 memberButton
-	       JButton memberButton = new JButton("会員");
+	       JButton memberButton = new JButton("会員管理");
 	       memberButton.addActionListener(e -> frame.showPanel("MEMBER"));
 	       
 	       //DVDパネルに移動　インスタンス名 dvdButton
-	       JButton dvdButton = new JButton("DVD");
+	       JButton dvdButton = new JButton("DVD管理");
 	       dvdButton.addActionListener(e -> frame.showPanel("DVD"));
 	      
 	       //貸出画面に移動　インスタンス名 lentButton
