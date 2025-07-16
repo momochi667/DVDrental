aa76e1598fd26ef0586917db7acc522fc2440389
mid <mid@k302.kcsk.ac.jp>
2025-07-16 09:45:37 +0900

    add

---
+++ src/jp/swing/dvdrental/MemberDeletePanel.java
@@ -0,0 +1,47 @@
+package jp.swing.dvdrental;
+
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class MemberDeletePanel extends JPanel {
+	public MemberDeletePanel(MainFrame frame) {
+        //レイアウトに新しいGridLayout(3, 2)
+    	setLayout(new GridLayout(3, 2));
+        
+        //IDのテキストフィールド
+    	JTextField idFieled = new JTextField();
+    	
+    	//削除ボタン
+    	JButton delBtn = new JButton("削除");
+    	
+    	//会員管理画面に戻るボタン
+    	JButton backBtn = new JButton("会員管理画面へ戻る");
+    	
+        //TOPに戻るボタン
+    	JButton topbackBtn = new JButton("TOPへ戻る");
+    	
+    	//削除ボタンリスナーで押されたらDBのインサート発動
+    	/*delBtn.addActionListener(e->{ 
+    		DB.insertMember(idFieled.getText());
+            JOptionPane.showMessageDialog(this, "削除が完了しました。");
+        });*/
+    	
+    	//会員管理画面に戻るボタン
+    	backBtn.addActionListener(e->frame.showPanel("MEMBER"));
+        
+        //TOPに戻るボタン
+        topbackBtn.addActionListener(e->frame.showPanel("TOP"));
+        
+        //カードに部品を追加
+        add(new JLabel("ID"));
+        add(idFieled);
+        add(delBtn);
+        add(backBtn);
+        add(topbackBtn);
+
+    }
+}
