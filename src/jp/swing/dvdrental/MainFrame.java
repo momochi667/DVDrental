dceb6cac6f83e557c49bb169145f74f4dacb1f18
mid <mid@k302.kcsk.ac.jp>
2025-07-16 09:48:33 +0900

    add

--- src/jp/swing/dvdrental/MainFrame.java
+++ src/jp/swing/dvdrental/MainFrame.java
@@ -11,7 +11,7 @@ public class MainFrame extends JFrame{
 	    private JPanel cardPanel;
 
 	    public MainFrame() {
-	        setTitle("図書館システム");
+	        setTitle("レンタルシステム");
 	        setDefaultCloseOperation(EXIT_ON_CLOSE);
 	        setSize(600, 400);
 
